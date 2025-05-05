1a1967bebb7cc02553644e1e8a2f7f1a6733be41
markharburg <harburg@gmail.com>
2025-05-05 10:30:42 -0700

    Add files via upload

---
+++ control.js
@@ -0,0 +1,11 @@
+const controlParams =
+{
+	'targetLanguage': 'ai',
+	'nativeLanguage': 'en',
+	'defaultAltOn': true,
+	'defaultNativeOn': true,
+	'defaultWordQuizOn': true,
+	'defaultSentQuizOn': true,
+	'defaultSkipNoQuizOn': false,
+	'defaultSpeakOn': true
+};
